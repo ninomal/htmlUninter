579e94cad3812b7d9ddb4666c29e5866f1cfcc8b
ninomal <137447782+ninomal@users.noreply.github.com>
2025-07-05 18:23:03 -0300

    first commit

---
+++ work1.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+  const divMensagem = document.getElementById("mensagem");
+  divMensagem.innerText = "Boas-vindas de Jonatas, futuro dev backend!";
+  divMensagem.style.marginTop = "10px";
+  divMensagem.style.fontSize = "18px";
+  divMensagem.style.color = "#6a1b9a"; // roxo escuro
+}
